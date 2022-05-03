13ff51e8461d47eb5c635b9ff84a19c329e3354a
Kyle Coberly <kyle.coberly@gmail.com>
2022-05-03 14:42:29 -0600

    Add solution

--- triangle.ts
+++ triangle.ts
@@ -1,1 +1,38 @@
-export class Triangle {}
+export class Triangle {
+  private readonly sides: number[];
+
+  constructor(...sides: number[]) {
+    this.sides = sides;
+  }
+
+  public get isValid(): boolean {
+    const [s1, s2, s3] = this.sides;
+    const sidesArePositive = s1 > 0 && s2 > 0 && s3 > 0;
+    const validatesTriangleInequality =
+      s1 + s2 >= s3 && s1 + s3 >= s2 && s2 + s3 >= s1;
+    return sidesArePositive && validatesTriangleInequality;
+  }
+
+  public get isEquilateral(): boolean {
+    if (!this.isValid) {
+      return false;
+    }
+    const [s1, s2, s3] = this.sides;
+    return s1 === s2 && s2 === s3 && s1 === s3;
+  }
+
+  public get isIsosceles(): boolean {
+    if (!this.isValid) {
+      return false;
+    }
+    const [s1, s2, s3] = this.sides;
+    return s1 === s2 || s1 === s3 || s2 === s3;
+  }
+
+  public get isScalene(): boolean {
+    if (!this.isValid) {
+      return false;
+    }
+    return !this.isIsosceles;
+  }
+}
