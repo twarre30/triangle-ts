52d6283e2f81ee52e17709a9c9ffdf653b80de58
Tiffany Warren <twarre30@gmail.com>
2022-05-06 17:52:51 -0500

    make a triangle, I think

--- triangle.ts
+++ triangle.ts
@@ -1,1 +1,17 @@
-export class Triangle {}
+export class Triangle {
+  sides: number[];
+
+  constructor(...sides: number[]) {
+    this.sides = sides;
+  }
+
+  get isValid(): boolean {
+    const [side1, side2, side3] = this.sides;
+    const sidesArePositive = side1 > 0 && side2 > 0 && side3 > 0;
+    const validatesTriangleInequality =
+      side1 + side2 >= side3 &&
+      side1 + side3 >= side2 &&
+      side2 + side3 >= side1;
+    return sidesArePositive && validatesTriangleInequality;
+  }
+}
